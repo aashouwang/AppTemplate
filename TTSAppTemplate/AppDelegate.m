ac07e76839fb4a6492697e20bf92f754c0297bd6
aashouwang <shouwangzhejuan@yahoo.cn>
2015-07-13 23:03:44 +0800

    新建模版工程

--- TTSAppTemplate/AppDelegate.m
+++ TTSAppTemplate/AppDelegate.m
@@ -7,12 +7,21 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    //这里设置RootViewController
+    
+    ViewController * rootVC = [[ViewController alloc] init];
+    self.window.rootViewController = rootVC;
+    [rootVC release];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 							
